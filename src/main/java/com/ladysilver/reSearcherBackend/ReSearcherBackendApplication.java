a2493eda46b823712035a52324cee0f70d7878b3
Layla <layla.silbernberg@appliedtechnology.se>
2023-11-22 15:40:49 +0100

    setting up env

---
+++ src/main/java/com/ladysilver/reSearcherBackend/ReSearcherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ladysilver.reSearcherBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSearcherBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReSearcherBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ladysilver/reSearcherBackend/ReSearcherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladysilver.reSearcherBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSearcherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
